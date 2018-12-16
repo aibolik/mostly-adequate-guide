349f7d222f5ee24c2bbe3060a4e2c3a163cb378b
Aibol Kussain <aibolikdev@gmail.com>
2018-12-16 21:22:55 +0300

    ch05: finish exercises related with Composing

--- exercises/ch05/exercise_c.js
+++ exercises/ch05/exercise_c.js
@@ -2,8 +2,16 @@
 // Hint, the `flip` function may come in handy.
 
 // fastestCar :: [Car] -> String
-const fastestCar = (cars) => {
-  const sorted = sortBy(car => car.horsepower, cars);
-  const fastest = last(sorted);
-  return concat(fastest.name, ' is the fastest');
-};
+// Given
+// const fastestCar = (cars) => {
+//   const sorted = sortBy(car => car.horsepower, cars);
+//   const fastest = last(sorted);
+//   return concat(fastest.name, ' is the fastest');
+// };
+
+const fastestCar = compose(
+  flip(concat, ' is the fastest'),
+  prop('name'),
+  last,
+  sortBy(prop('horsepower'))
+)
